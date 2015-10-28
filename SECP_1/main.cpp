de41689f1f3195f8b7cf87d9530e7bcba3951555
Daniel Fischer <fischer049@gmail.com>
2015-10-28 12:01:03 +0100

    Main

---
+++ SECP_1/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main(int argc, const char* argv[])
+{
+	// Prints each argument on the command line.
+	for (int i = 0; i < argc; i++)
+	{
+		printf("arg %d: %s\n", i, argv[i]);
+	}
+}
