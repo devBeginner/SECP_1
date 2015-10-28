3875982e9b5f61e1e6f6aad4c6fcf47d659aefaa
Daniel Fischer <fischer049@gmail.com>
2015-10-28 17:33:20 +0100

    Google und eigene Mod Funktion angepasst, für x mod x und die Ausgabe verschönert :)

--- SECP_1/main.cpp
+++ SECP_1/main.cpp
@@ -42,7 +42,7 @@ int moduloV2(int num, int divisor) {
 		return -1;
 	}
 
-	while (numAbs > divAbs) { numAbs -= divAbs; }
+	while (numAbs >= divAbs) { numAbs -= divAbs; }
 
 	if (num < 0) {
 		numAbs = -numAbs;
@@ -60,7 +60,7 @@ int moduloGoogle(int num, int divisor) {
 	divisor = abs(divisor);
 
 	if (numVorz == divVorz) {
-		while (num > divisor) { num -= divisor; }
+		while (num >= divisor) { num -= divisor; }
 	} else {
 		while (num > 0) { num -= divisor; }
 	}
@@ -70,22 +70,31 @@ int moduloGoogle(int num, int divisor) {
 
 int main()
 {
-	int C1 = 4;
-	int C2 = 3;
-	
+	// Hier kommen die Berechnungen aus der Aufgabenstellung c
+
+	int C1 = 87654;
+	int C2 = 13;
 	outputMod(C1, C2);
 	outputMod(-C1, C2);
 	outputMod(C1, -C2);
 	outputMod(-C1, -C2);
+	
+	outputMod(10, 10);
+
 
 	std::cin.get();
 	return 0;
 }
 
 void outputMod(int I1 ,int I2) {
-	cout << "\nNum:\t\t" << I1 << "\nDivisor:\t" << I2 << endl;
 	/*cout << "Version 1:\t" << getModulo(I1, I2) << endl;*/
-	cout << "% Own:\t\t" << moduloV2(I1, I2) << endl;
-	cout << "% Library:\t" << I1%I2 << endl;
-	cout << "% Google:\t" << moduloGoogle(I1, I2) << endl;
+	cout << endl;
+	cout << "--------------------" << endl;
+	cout << "| Num       | " << I1 << endl;
+	cout << "| Divisor   | " << I2 << endl;
+	cout << "--------------------" << endl;
+	cout << "| % Own     | " << moduloV2(I1, I2) << endl;
+	cout << "| % Library | " << I1%I2 << endl;
+	cout << "| % Google  | " << moduloGoogle(I1, I2) << endl;
+	cout << "--------------------" << endl;
 }
